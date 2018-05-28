ff240c59d7947c25cc6f281c1095c26b68830dc4
Sidderman <s.i.sidorovkrd@yandex.ru>
2018-05-28 18:06:30 +0300

    Паралакс, картинки, прикручена отправка формы AJAX.

--- src/index.php
+++ src/index.php
@@ -10,12 +10,20 @@
 <body>
 	<?php include ($_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'); ?>
 
+
+
 	<section class="promo" id="one">
+		<div class="paralax" id="paralax">
+			<div class="paralax__layer paralax__layer-1"></div>
+			<div class="paralax__layer paralax__layer-2"></div>
+			<div class="paralax__layer paralax__layer-3"></div>
+		</div>
+
 		<div class="container">
 			<div class="promo__box">
 				<h1 class="promo__title">А ты <br>точно <br>верстальщик?</h1>
 				<p class="promo__text">Мы тут подготовили для тебя несложное тестовое задание, при выполнении которого, ты сможешь проявить себя и доказать нам, что ты лучший!</p>
-				<a href="#" class="promo__btn">Да  легко!</a>
+				<a href="https://github.com/sidderman324/rnd-soft.test.ru" class="promo__btn">Да  легко!</a>
 			</div>
 		</div>
 	</section>
@@ -27,7 +35,7 @@
 			<ul class="task__list">
 				<li class="task__item">
 					<p class="task__number">1</p>
-					<p class="task__text">Нужно сделать по-красоте, вообщем,параллакс на элементах, на дяде и домиках при движении мышки</p>
+					<p class="task__text">Нужно сделать по-красоте, в общем,параллакс на элементах, на дяде и домиках при движении мышки</p>
 				</li>
 				<li class="task__item">
 					<p class="task__number">2</p>
@@ -47,11 +55,11 @@
 
 			<p class="feedback__text">Настрой форму обратной связи и отправь свои данные, когда закончишь с задачей=)</p>
 
-			<form action="/request_form.php" class="feedback__form">
-				<input type="text" name="" placeholder="Имя" class="feedback__input">
-				<input type="tel" name="" id="phone" placeholder="Телефон" class="feedback__input">
-				<input type="text" name="" placeholder="E-mail" class="feedback__input">
-				<input type="text" name="" placeholder="Время, потраченное на задачу" class="feedback__input">
+			<form action="/request_form.php" id="form_send" method="POST" class="feedback__form">
+				<input type="text" name="name" placeholder="Имя" class="feedback__input" required>
+				<input type="tel" name="phone" id="phone" placeholder="Телефон" class="feedback__input" required>
+				<input type="text" name="email" placeholder="E-mail" class="feedback__input" required>
+				<input type="text" name="time" placeholder="Время, потраченное на задачу" class="feedback__input" required>
 
 				<input type="submit" class="feedback__submit" value="Готово">
 			</form>
@@ -59,6 +67,11 @@
 	</section>
 
 	<?php include ($_SERVER['DOCUMENT_ROOT'] . '/modules/footer.php'); ?>
+
+	<div class="result">
+		<p class="result__text">123</p>
+		<span class="result__close"></span>
+	</div>
 
 	<script src='/js/script.min.js'></script>
 
--- src/js/script.js
+++ src/js/script.js
@@ -15,11 +15,76 @@ $(document).ready(function(){
 		});
 	});
 
-	// jQuery(window).on("scroll", function() {
-	// 	var scroll = jQuery(window).scrollTop();
-	// 	console.log(scroll);
+	jQuery('.burger__wrapper').on('click',function(){
+		jQuery('.main-menu').slideToggle(300);
+	});
+	jQuery('.main-menu__link').on('click',function(){
+		jQuery('.main-menu').slideToggle(300);
+	});
 
-	// 	if(scroll > 200) {jQuery('.page-header').css('position','fixed')}
-	// 		else {jQuery('.page-header').removeClass('page-header--fixed')}
-	// });
+	function menuHideShow() {
+		var windowsWidth = jQuery(window).width();
+		if (windowsWidth < 768) {
+			jQuery('.main-menu').css('display','none');
+		} else {
+			jQuery('.main-menu').css('display','flex');
+		}
+	};
+
+	jQuery(document).ready(menuHideShow);
+	jQuery(window).resize(menuHideShow);
+
+	jQuery('body').mousemove(function(e){
+		var x = (48 + e.pageX / jQuery(window).width() * 2);
+		var y = (48 + e.pageY / jQuery(window).height() * 2);
+		jQuery('.paralax__layer-1').css({'top':y + '%','left':x+'%'});
+
+		var x = (48 + e.pageX / jQuery(window).width() * 4);
+		var y = (48 + e.pageY / jQuery(window).height() * 4);
+		jQuery('.paralax__layer-2').css({'top':y + '%','left':x+'%'});
+
+		var x = (48 + e.pageX / jQuery(window).width() * 6);
+		var y = (48 + e.pageY / jQuery(window).height() * 6);
+		jQuery('.paralax__layer-3').css({'top':y + '%','left':x+'%'});
+	});
+
+	if (jQuery(window).width() < 768) {
+		function onOrientationChange(e) {
+			var parX = Math.round(e.gamma);
+			var parY = Math.round(e.beta);
+			var x = 50 + parX / 5;
+			var y = 100 + parY / 5;
+			// jQuery('#click').text('parX: '+ x + 'parY: '+ parY);
+			jQuery('.paralax').css('background-position', x + '% ' + y + '%');
+		}
+		window.addEventListener('deviceorientation', onOrientationChange);
+	}
+
+	jQuery('.result__close').on('click',function(){
+		jQuery('.result').fadeOut(200);
+	});
+
+	$(function(){
+		$('#form_send').submit(function(e){
+			e.preventDefault();
+			var m_method=$(this).attr('method');
+			var m_action=$(this).attr('action');
+			var m_data=$(this).serialize();
+			$.ajax({
+				type: m_method,
+				url: m_action,
+				data: m_data,
+				success: function(){
+					jQuery('.result').fadeIn(200);
+					jQuery('.result__text').text('Ваше сообщение успешно отправлено');
+					document.getElementById('form_send').reset();
+				},
+				error: function(){
+					jQuery('.result').fadeIn(200);
+					jQuery('.result__text').text('При отправке произошла ошибка');
+					document.getElementById('form_send').reset();
+				}
+			});
+		});
+	});
 });
